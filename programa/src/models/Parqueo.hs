d2efac843c091ac2da14bce66e92edfadc462666
Alex Sánchez C <asmalexander@hotmail.com>
2022-09-24 22:47:03 -0600

    Funcion mostrar parqueos por provincia y documentacion
    Se agrego una funcion para mostrar parqueos a apartir de una provincia y documentacion total del archivo

--- programa/src/models/Parqueo.hs
+++ programa/src/models/Parqueo.hs
@@ -1,15 +1,12 @@
 {-# LANGUAGE DeriveGeneric #-}
-
 module Parqueo where
 
+{-Importacion de librerias-}
 import Data.Aeson
 import Data.Aeson.Encode.Pretty
 import GHC.Generics
 import qualified Data.ByteString.Lazy as BS
 
--- de momento -- 
-path_parqueo :: FilePath
-path_parqueo = "./src/data/parqueo.json"
 
 {--------------------------------------------------------------------------------------------------
 Modelo: Parqueo
@@ -36,6 +33,13 @@ data Parqueo = Parqueo {
 instance ToJSON Parqueo
 instance FromJSON Parqueo
 
+
+{-
+    Nombre: obtenerParqueos
+    Descripcion: funcion que obtiene la informacion de los parqueos de un archivo externo
+    Entradas: nombre del archivo
+    Salidas: lista con la informacion de los parqueos
+-}
 obtenerParqueos :: String -> IO [Parqueo]
 obtenerParqueos path = do
     contenido <- BS.readFile path
@@ -43,30 +47,81 @@ obtenerParqueos path = do
     case parqueos of
         Just parqueos -> return parqueos
         Nothing -> return []
-
+        
+{-
+    Nombre: existeParqueo
+    Descripcion: funcion que verifica si existe el nombre de un parqueo
+    Entradas: nombre del archivo - nombre del parqueo
+    Salidas: valor booleano
+-}
 existeParqueo :: String -> String -> IO Bool
 existeParqueo path nombre = do
     parqueos <- obtenerParqueos path
     return (existeParqueo' parqueos nombre :: Bool)
 
+{-
+    Nombre: existeParqueo'
+    Descripcion: funcion auxiliar de existeParqueo
+    Entradas: nombre del archivo - nombre del parqueo
+    Salidas: valor booleano
+-}
 existeParqueo' :: [Parqueo] -> String -> Bool
 existeParqueo' [] nombre = False
 existeParqueo' (x:xs) nombre = if (Parqueo.nombre x) == nombre then True else existeParqueo' xs nombre
 
-
+{-
+    Nombre: mostrarParqueo
+    Descripcion: funcion que genera la cadena de informacion de los parqueos
+    Entradas: un objeto de tipo Parqueo
+    Salidas: un string
+-}
 mostrarParqueo :: Parqueo -> String
 mostrarParqueo parqueo = "Nombre del Parqueo: " ++ (Parqueo.nombre parqueo) ++ "\n" ++
-                             "Direccion del Parqueo: " ++ (Parqueo.direccion parqueo) ++ "\n" ++
-                             "Provincia del Parqueo: " ++ (Parqueo.provincia parqueo) ++ "\n" ++
-                             "Coordenadas del Parqueo(x,y): (" ++ show (x parqueo) ++ ","++ show (y parqueo) ++ ")"
+    "Direccion del Parqueo: " ++ (Parqueo.direccion parqueo) ++ "\n" ++
+    "Provincia del Parqueo: " ++ (Parqueo.provincia parqueo) ++ "\n" ++
+    "Coordenadas del Parqueo(x,y): (" ++ show (x parqueo) ++ ","++ show (y parqueo) ++ ")"
 
+{-
+    Nombre: mostrarParqueos
+    Descripcion: funcion que imprime los datos de los parqueos
+    Entradas: la ubicacion del archivo
+    Salidas: sin salidas
+-}
 mostrarParqueos :: String -> IO ()
 mostrarParqueos path = do
     parqueos <- obtenerParqueos path
     putStrLn (mostrarParqueos' parqueos)
 
+{-
+    Nombre: mostrarParqueos'
+    Descripcion: funcion auxiliar de mostrarParqueos
+    Entradas: lista con la informacion de todos los parqueos
+    Salidas: un string
+-}
 mostrarParqueos' :: [Parqueo] -> String
 mostrarParqueos' [] = ""
-mostrarParqueos' (x:xs) = (mostrarParqueo x) ++ 
-                            "\n-----------------------------------------------------\n" ++
-                            (mostrarParqueos' xs)
+mostrarParqueos' (x:xs) = (mostrarParqueo x) ++ "\n--------------------------------------------------------------------------------------------------\n" ++(mostrarParqueos' xs)
+
+{-
+    Nombre: mostrarParqueosPorProvincia
+    Descripcion: funcion que imprime los parqueos apartir de una provincia
+    Entradas: ubicacion del archivo - nombre de la provincia
+    Salidas: un string
+-}
+mostrarParqueosPorProvincia :: String -> String -> IO ()
+mostrarParqueosPorProvincia path provincia = do
+    parqueos <- obtenerParqueos path
+    putStrLn (mostrarParqueosPorProvincia' parqueos provincia)
+
+{-
+    Nombre: mostrarParqueosPorProvincia'
+    Descripcion: funcion auxiliar de mostrarParqueosPorProvincia
+    Entradas: lista con la informacion de los parqueos - nombre de la provincia
+    Salidas: un string
+-}
+mostrarParqueosPorProvincia' :: [Parqueo] -> String -> String
+mostrarParqueosPorProvincia' [] provincia = ""
+mostrarParqueosPorProvincia' (x:xs) provincia = 
+    if (Parqueo.provincia x) == provincia then (mostrarParqueo x) ++ "\n-----------------------------------------------------\n" ++
+        (mostrarParqueosPorProvincia' xs provincia) 
+    else mostrarParqueosPorProvincia' xs provincia
